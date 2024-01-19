1858b577f60df1906b622cadfe9b97aeb56e2858
Kenan Khauto <kenan.khauto@outlook.com>
2024-01-19 12:02:04 +0100

    added detector usage notebook

--- models/utils.py
+++ models/utils.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import matplotlib.pyplot as plt
 
 def count_trainable_parameters(model):
     """
@@ -14,3 +14,34 @@ def count_trainable_parameters(model):
         int: The number of trainable parameters in the model.
     """
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def show_training(history):
+    """
+    Show loss and accuracies during training.
+
+    Parameters:
+        - history (dict):
+            - loss (list[float]): Training losses.
+            - train_acc (list[float]): Training accuracies.
+            - val_acc (list[float]): Validation accuracies.
+
+    """
+    fig, (lhs, rhs) = plt.subplots(ncols=2, figsize=(12, 4))
+    fig.suptitle('Training')
+
+    # Set subplot titles.
+    lhs.set_title('Loss')
+    rhs.set_title('mAP')
+
+    # Set subplot axis labels.
+    lhs.set_xlabel('epoch'), lhs.set_ylabel('loss')
+    rhs.set_xlabel('epoch'), rhs.set_ylabel('mAP')
+
+    # Plot loss and accuracies.
+    lhs.plot(history['loss'])
+    rhs.plot(history['train_accuracy'], label='train')
+    rhs.plot(history['val_accuracy'], label='val')
+    rhs.legend()
+
+    plt.show()

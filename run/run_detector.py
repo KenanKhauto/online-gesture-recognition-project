be14333e40824b45f3c7de413ac1c5e26d17915d
Kenan Khauto <kenan.khauto@outlook.com>
2024-01-27 13:43:09 +0100

    path cahnged for detector

--- run/run_detector.py
+++ run/run_detector.py
@@ -6,5 +6,5 @@ ann_test = "/scratch/vihps/vihps14/IPN_Hand/annotations/Annot_TestList.txt"
 save_path = "/scratch/vihps/vihps14/IPN_Hand/models/resnetl_detector_V2.pth"
 #process_id = int(os.environ.get("SLURM_PROCID"))
 
-os.system(f"python train_detector.py {frames_path} {ann_train} {ann_test} {save_path}")
+os.system(f"python ./train/train_detector.py {frames_path} {ann_train} {ann_test} {save_path}")
 
